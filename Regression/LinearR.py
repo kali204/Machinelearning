f3dd46777774914723a3ed5f019a86c2658de7b9
kali204 <abhaystark77@gmail.com>
2023-09-19 22:05:01 +0530

    add graph

--- Regression/LinearR.py
+++ Regression/LinearR.py
@@ -1,11 +1,12 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error
 
 diabetes = datasets.load_diabetes()
 # dict_keys(['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'])
-diabetes_X=diabetes.data[:,np.newaxis,2]
+# diabetes_X=diabetes.data[:,np.newaxis,2]
+diabetes_X=diabetes.data
 diabetes_X_train=diabetes_X[:-30]
 
 diabetes_X_test=diabetes_X[-30:]
@@ -15,7 +16,14 @@ diabetes_y_test=diabetes.target[-30:]
 
 model=linear_model.LinearRegression()
 
-model.fit(diabetes_X_train,diabetes_X_train)
+model.fit(diabetes_X_train,diabetes_y_train)
 diabetes_y_predicted=model.predict(diabetes_X_test)
 
 print("Mean Squared error is : ",mean_squared_error(diabetes_y_test,diabetes_y_predicted))
+print("Weights: ",model.coef_)
+print("Intercept: ",model.intercept_)
+
+# plt.scatter(diabetes_X_test,diabetes_y_test)
+# plt.plot(diabetes_X_test,diabetes_y_predicted)
+
+# plt.show()
